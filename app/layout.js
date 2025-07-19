d9980ea4cc33adb3ff584602983a0aafe7871ceb
Satyam8589 <satyamkumarsingh9898@gmail.com>
2025-07-19 13:45:14 +0530

    upto Dashboard is completed

---
+++ app/layout.js
@@ -0,0 +1,39 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import React from "react";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Splitr",
+  description: "The smartest way to split expenses with friends",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+      </head>
+      <body
+        className={`${inter.className}`}
+      >
+        <ClerkProvider>
+          <ConvexClientProvider>
+            <Header />
+            <main className="min-h-screen">
+              <Toaster richColors />
+
+              {children}
+              <Toaster richColors />
+            </main>
+          </ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
